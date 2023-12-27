4d2f3b2a8a02978f720aef2c4998544558d1becc
Samaksh <Samaksh13kaushik@gmail.com>
2023-12-27 09:32:46 +0100

    deleted icon

--- src/components/common/logo.jsx
+++ src/components/common/logo.jsx
@@ -6,21 +6,21 @@ import INFO from "../../data/user";
 import "./styles/logo.css";
 
 const Logo = (props) => {
-	let { width, link } = props;
+// 	let { width, link } = props;
 
-	if (link === undefined) {
-		link = true;
-	}
+// 	if (link === undefined) {
+// 		link = true;
+// 	}
 
-	const imageElement = (
-		<img src={INFO.main.logo} alt="logo" className="logo" width={width} />
-	);
+// 	const imageElement = (
+// 		<img src={INFO.main.logo} alt="logo" className="logo" width={width} />
+// 	);
 
-	return (
-		<React.Fragment>
-			{link ? <Link to="/">{imageElement}</Link> : imageElement}
-		</React.Fragment>
-	);
-};
+// 	return (
+// 		<React.Fragment>
+// 			{link ? <Link to="/">{imageElement}</Link> : imageElement}
+// 		</React.Fragment>
+// 	);
+ };
 
 export default Logo;
